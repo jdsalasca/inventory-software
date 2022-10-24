907bda89d3209936c51bcd2da707ca4d06926a79
jdsalasca <jdsalasc@unal.edu.co>
2022-10-24 16:44:17 -0500

    improving responsive on landing page

--- src/components/HomePage/Index.js
+++ src/components/HomePage/Index.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Carousel } from "react-bootstrap";
 import "./scss/index.scss";
-import "./scss/landingpage.scss"
+import "./scss/landingpage.scss";
 export const Index = () => {
-
-
+  const [index, setIndex] = useState(0);
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
   const welcome = (
-    <div className="col-6 w-100  rounded container mt-5 shadow p-3 mb-5 bg-white div-landing-page-card">
+    <div className="div-landing-page-card">
       <h1 className="display-1 fw-bold">Te damos la bienvenida</h1>
-      <p className="fst-italic">
-        El trabajo en equipo puede ser difiicil, desordendo,
-        complicado...pero sigue siendo la mejor forma de trabajar. Por eso
-        creamos Nuestros software un lugar donde las personas trabajan
-        juntas
+      <p className="fst-italic">El trabajo en equipo puede ser difiicil, desordendo, complicado...pero
+        sigue siendo la mejor forma de trabajar. Por eso creamos Nuestros
+        software un lugar donde las personas trabajan juntas
       </p>
       <div className="div-landing-page-buttons-options mx-5 px-3">
         <button type="button w-25 rounded" className="btn btn-success">
@@ -23,48 +23,35 @@ export const Index = () => {
         </button>
       </div>
     </div>
-  )
-  return (
-    <div className="div-parent row">
-      <div className="bg-image">
-        <Carousel
-        variant='dark'
-        className ="
-        d-flex
-        bg-light
-        row mx-5 mt-5 rounded col- h-50 landing-page-carrousel"  
+  );
+  const className = "container row col align-self-center";
+  return (  
+    <div className="div-parent">
+      <div className="bg-image d-flex justify-content-center">
+      <Carousel activeIndex={index} onSelect={handleSelect}
+        slide={false}
+        pause= {'hover'}
+          variant="dark"  
+          className="bg-light shadow container p-4 col-6 align-self-center rounded mt-4 landing-page-carrousel"
         >
-          <Carousel.Item
-            className="container row col-6 w-75 bg-black mx-0"
+          <Carousel.Item 
+          interval={100000}
+          className={className}>{welcome}</Carousel.Item>
 
-          >
-            <h3>Third slidAAAAAAAAAAAAAAAAAAAAAAAAAAAe label</h3>
+          <Carousel.Item className={className}>
+            <h3>second slide label</h3>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
           </Carousel.Item>
-
-          <Carousel.Item
-            className="container row col-6 w-75 bg-black mx-0"
-          >
-            <div
-            >sssss
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-          </Carousel.Item>
-          <Carousel.Item
-            className="container row col-6 w-75 bg-black mx-0"
-          >
+          <Carousel.Item className={className}>
             <h3>Third slide label</h3>
             <p>
               Praesent commodo cursus magna, vel scelerisque nisl consectetur.
             </p>
           </Carousel.Item>
         </Carousel>
-
       </div>
-
     </div>
   );
 };
